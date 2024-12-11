e3b96d8e3ffbaca54588d2b8d1057e17577ff3d8
hibbes <marek@czernohous.de>
2024-12-11 09:01:12 +0100

    Probedivision

---
+++ Prime.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Prime {
+
+    public static void main(String[] args) {
+
+        System.out.println(output(generatePrimes(100)));
+    }
+
+    public static boolean isPrime(int candidate) {
+        for (int divider = 2; divider <= Math.sqrt(candidate); divider++) {
+            if (candidate % divider == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static List<Integer> generatePrimes(int max) {
+        List<Integer> primes = new ArrayList<>();
+        for (int candidate = 2; candidate <= max; candidate++) {
+            if (isPrime(candidate)) {
+                primes.add(candidate);
+            }
+        }
+        return primes;
+    }
+
+    public static String output(List<Integer> primes) {
+        String output = "";
+        for (Integer prime : primes) {
+            output = output + " " + prime;
+        }
+        return output;
+    }
+
+}
