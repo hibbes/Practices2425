c5d53c82a5a5945f5e5a4637a37e87c5f02e5d5e
hibbes <marek@czernohous.de>
2024-12-17 07:26:44 +0100

    inital commit

--- Probedivision.java
+++ Probedivision.java
@@ -1,11 +1,16 @@
 public class Probedivision {
+
+static int calc = 0;
     // Methode zur Überprüfung, ob eine Zahl prim ist
     public static boolean isPrime(int number) {
+
         if (number <= 1) return false; // Zahlen ≤ 1 sind keine Primzahlen
         // Schleife von 2 bis zur Quadratwurzel der Zahl
         for (int divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+            calc++;
             // TODO: Implementiere die Teilbarkeitsprüfung
             if (number % divisor == 0) {
+
                 return false; // Zahl ist durch divisor teilbar, also keine Primzahl
             }
         }
@@ -13,13 +18,18 @@ public class Probedivision {
     }
 
     public static void main(String[] args) {
-        int limit = 1000000; // Obergrenze für die Primzahlsuche
+        int counter = 0;
+        int limit = 200; // Obergrenze für die Primzahlsuche
         // Schleife von 2 bis zur Obergrenze
         for (int currentNumber = 2; currentNumber <= limit; currentNumber++) {
             // TODO: Rufe isPrime auf und gib die Primzahlen aus
             if (isPrime(currentNumber)) {
+                counter++;
                 System.out.print(currentNumber + " ");
-            }
+            }           
         }
+        System.out.println("\nPrimzahlen: "+counter);
+        System.out.println("Rechenoperationen: "+calc);
+
     }
 }
--- SieveOfEratosthenes.java
+++ SieveOfEratosthenes.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 
 public class SieveOfEratosthenes {
+static int calc = 0;
+
+
     // Methode zur Implementierung des Siebs des Eratosthenes
     public static boolean[] sieveOfEratosthenes(int limit) {
         boolean[] isPrime = new boolean[limit + 1];
@@ -12,6 +15,7 @@ public class SieveOfEratosthenes {
             if (isPrime[currentPrime]) {
                 // TODO: Implementiere das Markieren der Vielfachen von currentPrime
                 for (int multiple = currentPrime * currentPrime; multiple <= limit; multiple += currentPrime) {
+                    calc++;
                     if (isPrime[multiple]) {
                         isPrime[multiple] = false; // Markiere Vielfaches als nicht prim
                         // Optional: Weitere Aktionen bei der Markierung
@@ -23,15 +27,19 @@ public class SieveOfEratosthenes {
     }
 
     public static void main(String[] args) {
-        int limit = 1000000; // Obergrenze für die Primzahlsuche
+        int counter=0;
+        int limit = 200; // Obergrenze für die Primzahlsuche
         boolean[] primes = sieveOfEratosthenes(limit);
 
         // Schleife von 2 bis zur Obergrenze, um Primzahlen auszugeben
         for (int number = 2; number <= limit; number++) {
             // TODO: Gib die Primzahlen aus
             if (primes[number]) {
-                System.out.println(number + " ist eine Primzahl.");
+                counter++;
+                System.out.print(number + " ");
             }
         }
+        System.out.println("\nPrimzahlen: "+counter);
+        System.out.println("Rechenoperationen: "+calc);
     }
 }
